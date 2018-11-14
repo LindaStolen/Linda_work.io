669dc37949c65aeca037acfbb8541e3048a790eb
Linda <plusmeow218@gmail.com>
2018-11-14 08:18:49 +0800

    order manage

--- ordersystem/js/sign-in.js
+++ ordersystem/js/sign-in.js
@@ -1,5 +1,9 @@
 $(function(){
     $(".signIn").attr("style","color: #0d00c0");
     $(".late").attr("style","color: #c00000");
+
+    $("#user").click(function(){
+        $(".user").slideToggle().show();
+    })
 });
 
