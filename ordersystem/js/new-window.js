4b95813fed8a5f6c97338ede51ac57e9ec46e8ad
Linda <plusmeow218@gmail.com>
2018-11-21 17:46:43 +0800

    newwindow

--- ordersystem/js/new-window.js
+++ ordersystem/js/new-window.js
@@ -1,1 +1,4 @@
-window.open('視窗連結','修改預約資料',config='height=500px;weight=500px;')
+function openNew(){
+    let nw;
+    nw = window.open("change-new.html","修改預約","toolbar=no,location=no,left=500px,width=600,height=500");
+}
---
+++ ordersystem/js/showData.js
@@ -0,0 +1,13 @@
+// place-change
+// $(document).ready(function(){
+//     $('.submit').click(function(){
+//         alert('done');
+//         $('#table_2').show();
+//     })
+// })
+$('#table_2').hide();
+function showdata(){
+    alert();
+    $('#table_2').show();
+}
+console.log(showdata());
