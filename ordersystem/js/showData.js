84d8dc7f1f409d33ff90bdf33bcbc8ea591b6764
Linda <plusmeow218@gmail.com>
2018-11-22 17:39:57 +0800

    show().js

--- ordersystem/js/showData.js
+++ ordersystem/js/showData.js
@@ -1,13 +1,30 @@
-// place-change
-// $(document).ready(function(){
-//     $('.submit').click(function(){
-//         alert('done');
-//         $('#table_2').show();
-//     })
-// })
-$('#table_2').hide();
-function showdata(){
-    alert();
-    $('#table_2').show();
-}
-console.log(showdata());
+// place-manage&change 
+$(document).ready(function(){
+    $('.submit').click(function(){
+        alert('setting is done!');
+        $("#table_2").show();
+        return false;
+    })
+
+    //新增預約
+    $('.btn').click(function(){
+        $(".crossLine").show();
+        $(".student_info").show();
+        return false;
+    })
+    $('.reset').click(function(){
+        $("input").val(date).empty();
+        $("select").empty();
+    })
+
+    // 修改刪除預約
+    $('.btn_correct').click(function(){
+        $('.student_info').show();
+        $('.data').hide();
+    })
+})
+// function showdata(){
+//     alert('setting is done!');
+//     $("#table_2").show();
+//     return false;
+// }
