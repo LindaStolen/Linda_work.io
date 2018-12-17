050ec2c4ab66aca7776ae9ecd819f055c1e70a02
Linda <plusmeow218@gmail.com>
2018-12-17 17:52:50 +0800

    statistic.html

--- ordersystem/js/highcharts/charts.js
+++ ordersystem/js/highcharts/charts.js
@@ -1,9 +1,42 @@
-var chart = c3.generate({
-    bindto: '.clac',
-    data: {
-      columns: [
-        ['data1', 30, 200, 100, 400, 150, 250],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ]
-    }
-});
+// google.charts.load('current', {packages: ['corechart', 'bar']});
+// google.charts.setOnLoadCallback(drawBarColors);
+
+// function drawBarColors() {
+//     var data = google.visualization.arrayToDataTable([
+//         ['City', '2010 Population', '2000 Population'],
+//         ['New York City, NY', 8175000, 8008000],
+//         ['Los Angeles, CA', 3792000, 3694000],
+//         ['Chicago, IL', 2695000, 2896000],
+//         ['Houston, TX', 2099000, 1953000],
+//         ['Philadelphia, PA', 1526000, 1517000]
+//         ]);
+
+//     var options = {
+//         title: '預約實到統計',
+//         chartArea: {width: '150px'},
+//         colors: ['#b0120a', '#ffab91'],
+//         hAxis: {
+//         title: 'Total Population',
+//         minValue: 0
+//         },
+//         vAxis: {
+//         title: 'City'
+//         }
+//     };
+//     var chart = new google.visualization.BarChart(document.getElementById('chart_'));
+//     chart.draw(data, options);
+// }
+
+// chartJS
+
+let ctx = $('#chart');
+let chart = new Chart(ctx,{
+  type: 'bar',
+  data: {
+    labels: ['12/17','12/18','12/19','12/20','12/21','12/22','12/23'],
+    datasets: [{
+      lable: '目前一周預約實到(人數)',
+    }]
+  }
+
+})
