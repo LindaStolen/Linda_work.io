914fd0f7070d10a6fe408ba4399195019ab70cd9
Linda <plusmeow218@gmail.com>
2018-12-24 17:37:35 +0800

    input checked

---
+++ ordersystem/js/chosen.js
@@ -0,0 +1,4 @@
+$(function(){
+    $(".chosen-container").width("300px");
+    $(".chosen-search-input").width("100%");
+});
--- ordersystem/js/table_add.js
+++ ordersystem/js/table_add.js
@@ -1,13 +1,35 @@
 let table = $('table#group');
-let input = $('#groupName');
-let gName = $(input).val();
-console.log(table);
-console.log(input);
-console.log(gName);
+let item = $("#groupName");
+let gName = $(item).val();
+let check = $("input:checked");
+$("input[type=checkbox]").change(function(){
+    let value = $("input:checked").val();
+    let str = $(".show").text();
+    console.log(str);
+    
+    if (check){
+        $(this).prop("check","checked");
+        str += value;
+        $(".show").text(str + " , ");
+    }else {
+        $(".show").slice();
+    }
+    // str += value;
+    // $(".show").text(str + " , ");
+});
 
-$('button').click(function(){
-    table.append('<tr><td class="g-name"></td><td class="p-item"></td><td class="edit"></td></tr>');
-    $('td.g-name').append(gName);
+
+
+$('button').click(function(str){
+    // let pItem = $(".show").val();
+    // let pItem = $('input[type=checkbox]').val();
+    let str_ = `<tr><td class="g-name">${gName}</td><td class="p-item">${pItem}</td><td class="edit"><button class="revise">編輯</button><br><button class="no">刪除</button></td></tr>`
+    table.append(str_);
+    item.val("");
+
+    
+    // console.log(pItem);
+
     return false;
 });
 // console.log($('button'));
