8c811972d78dd3ad037ca63026b2770078a4e61c
Linda <plusmeow218@gmail.com>
2018-12-27 18:03:00 +0800

    update checkbox

--- ordersystem/js/table_add.js
+++ ordersystem/js/table_add.js
@@ -1,35 +1,60 @@
 let table = $('table#group');
 let item = $("#groupName");
-let gName = $(item).val();
-let checked = '';
+// let checked = '';
+let p; //所選的checkbox
+let str = '';
+let arr = new Array();
+let new_str = '';
 $("input[type='checkbox']").change(function(){
-    let p = $(this).attr('checked',true);
     if($(this).prop('checked')){
-        checked = $(this).val();
-        console.log(checked);
-        let str = $(".show").html();
-        str +=  $(this).val() + " / ";
+        p = $(this).attr('checked',true);
+        str += $(this).parent().text() + ",";
         $(".show").text(str);
     }else {
-        $(this).removeAttr('checked');
-        $(".show").remove($(this).val());
+        new_str = '';
+        p.removeAttr('checked');
+        str = str.substring(0, str.length-1);
+        arr = str.split(",");
+        for (let i = 0;i< arr.length;i++){
+            if($(this).parent().text() == arr[i]){ //所選的陣列位置的值
+                console.log("使用者選取的值："+ arr[i]);
+                arr = ''; // 取消陣列值
+            }
+            new_str += arr[i] + ",";
+            if(i==0){
+                str = arr[i];
+            }else{
+                str += arr[i];
+            }
+        }
+        
+        $(".show").text(new_str);
     }
 });
+
 $(".cancel").click(function(){
     $("input[type='checkbox']").prop('checked',false);
     $("input[type='checkbox']").removeAttr('checked');
+    str = '';
+    new_str = '';
     $(".show").empty();
     // console.log($("input:checked"));
 });
 
 
 
-$('submit').click(function(){
-    let pItem = $(".show").val();
-    // let pItem = $('input[type=checkbox]').val();
+$('.submit').click(function(){
+    $("input[type='checkbox']").prop('checked',false);
+    $("input[type='checkbox']").removeAttr('checked');
+    let pItem = ($(".show").html());
+    let gName = $(item).val();    
+    // console.log(gName);
     let str_ = `<tr><td class="g-name">${gName}</td><td class="p-item">${pItem}</td><td class="edit"><button class="revise">編輯</button><br><button class="no">刪除</button></td></tr>`
     table.append(str_);
-    item.val("");
+    $(item).val("");
+    str = '';
+    new_str = '';
+    $(".show").empty();
 
     return false;
 });
