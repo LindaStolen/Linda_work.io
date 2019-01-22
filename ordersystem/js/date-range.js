c42bf14f2006d8b34e36b1c2cd6cc98d2eccf915
Linda <plusmeow218@gmail.com>
2019-01-22 17:49:46 +0800

    ajax.html

--- ordersystem/js/date-range.js
+++ ordersystem/js/date-range.js
@@ -58,3 +58,33 @@ $("#date-range-search").on('cancel.daterangepicker', function(ev, picker) {
     //do something, like clearing an input
     $("#date-range-search").val('');
 });
+//查詢學習時數記錄 
+$(".date-range-search").daterangepicker({
+    autoUpdateInput: false,
+    locale: {
+        format: "YYYY-MM-DD",
+        separator: " ~ ",
+        applyLabel: "確定",
+        cancelLabel: "清除",
+        customRangeLabel: "自訂日期區間",
+        daysOfWeek: ["日", "一", "二", "三", "四", "五", "六"],
+        monthNames: ["1月", "2月", "3月", "4月", "5月", "6月",
+        "7月", "8月", "9月", "10月", "11月", "12月"
+        ],
+        firstDay: 1
+        },
+        minDate: moment().subtract(18, 'month'),
+        maxDate: moment(),
+        autoApply: false,
+        showDropdowns: true,
+},function(start, end, label) {
+    console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+  });
+  
+$(".date-range-search").on("apply.daterangepicker", function(ev, picker) {
+    $(this).val(picker.startDate.format("YYYY-MM-DD") + " ~ " + picker.endDate.format("YYYY-MM-DD"));
+});
+$(".date-range-search").on('cancel.daterangepicker', function(ev, picker) {
+    //do something, like clearing an input
+    $(".date-range-search").val('');
+});
---
+++ ordersystem/test.php
@@ -0,0 +1,7 @@
+<?php 
+//   $user = $_POST["user"];
+//   $mail = $_POST["mail"];
+  
+  echo $_POST["user"];
+  echo $_POST["mail"];
+?>
