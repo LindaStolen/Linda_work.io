e3349ab49968179db764e3f5f08b975699a6f9de
Linda <plusmeow218@gmail.com>
2019-05-28 12:34:51 +0800

    catagory-style

--- material/js/bootstrap-submenu.js
+++ material/js/bootstrap-submenu.js
@@ -1,4 +1,4 @@
-(function(factory) {
+(function (factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module
     define(['jquery'], factory);
@@ -9,34 +9,49 @@
     // Browser globals
     factory(jQuery);
   }
-})(function($) {
-  class DropdownSubmenu {
-    constructor(element) {
+})(function ($) {
+  var DropdownSubmenu =
+  /*#__PURE__*/
+  function () {
+    function DropdownSubmenu(element) {
       this.element = element.parentElement;
       this.menuElement = this.element.querySelector('.dropdown-menu');
-
       this.init();
     }
 
-    init() {
+    var _proto = DropdownSubmenu.prototype;
+
+    _proto.init = function init() {
+      var _this = this;
+
       $(this.element).off('keydown.bs.dropdown.data-api');
-
       this.menuElement.addEventListener('keydown', this.itemKeydown.bind(this));
+      var dropdownItemNodeList = this.menuElement.querySelectorAll('.dropdown-item');
 
-      const dropdownItemNodeList = this.menuElement.querySelectorAll('.dropdown-item');
+      for (var _iterator = dropdownItemNodeList, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
+        var _ref;
 
-      for (const element of dropdownItemNodeList) {
+        if (_isArray) {
+          if (_i >= _iterator.length) break;
+          _ref = _iterator[_i++];
+        } else {
+          _i = _iterator.next();
+          if (_i.done) break;
+          _ref = _i.value;
+        }
+
+        var element = _ref;
         element.addEventListener('keydown', this.handleKeydownDropdownItem.bind(this));
       }
 
       $(this.menuElement).on('keydown', '.dropdown-submenu > .dropdown-item', this.handleKeydownSubmenuDropdownItem.bind(this));
       $(this.menuElement).on('click', '.dropdown-submenu > .dropdown-item', this.handleClickSubmenuDropdownItem.bind(this));
-      $(this.element).on('hidden.bs.dropdown', () => {
-        this.close(this.menuElement);
+      $(this.element).on('hidden.bs.dropdown', function () {
+        _this.close(_this.menuElement);
       });
-    }
+    };
 
-    handleKeydownDropdownItem(event) {
+    _proto.handleKeydownDropdownItem = function handleKeydownDropdownItem(event) {
       // 27: Esc
       if (event.keyCode !== 27) {
         return;
@@ -44,40 +59,35 @@
 
       event.target.closest('.dropdown-menu').previousElementSibling.focus();
       event.target.closest('.dropdown-menu').classList.remove('show');
-    }
+    };
 
-    handleKeydownSubmenuDropdownItem(event) {
+    _proto.handleKeydownSubmenuDropdownItem = function handleKeydownSubmenuDropdownItem(event) {
       // 32: Spacebar
       if (event.keyCode !== 32) {
         return;
-      }
+      } // NOTE: Off vertical scrolling
 
-      // NOTE: Off vertical scrolling
+
       event.preventDefault();
-
       this.toggle(event.target);
-    }
+    };
 
-    handleClickSubmenuDropdownItem(event) {
+    _proto.handleClickSubmenuDropdownItem = function handleClickSubmenuDropdownItem(event) {
       event.stopPropagation();
-
       this.toggle(event.target);
-    }
+    };
 
-    itemKeydown(event) {
+    _proto.itemKeydown = function itemKeydown(event) {
       // 38: Arrow up, 40: Arrow down
       if (![38, 40].includes(event.keyCode)) {
         return;
-      }
+      } // NOTE: Off vertical scrolling
 
-      // NOTE: Off vertical scrolling
+
       event.preventDefault();
-
       event.stopPropagation();
-
-      const itemNodeList = this.element.querySelectorAll('.show > .dropdown-item:not(:disabled):not(.disabled), .show > .dropdown > .dropdown-item');
-
-      let index = Array.from(itemNodeList).findIndex((element) => {
+      var itemNodeList = this.element.querySelectorAll('.show > .dropdown-item:not(:disabled):not(.disabled), .show > .dropdown > .dropdown-item');
+      var index = Array.from(itemNodeList).findIndex(function (element) {
         return element === event.target;
       });
 
@@ -90,39 +100,49 @@
       }
 
       itemNodeList[index].focus();
-    }
+    };
 
-    toggle(element) {
-      const dropdownElement = element.closest('.dropdown');
-      const parentMenuElement = dropdownElement.closest('.dropdown-menu');
-      const menuElement = dropdownElement.querySelector('.dropdown-menu');
-      const isOpen = menuElement.classList.contains('show');
-
+    _proto.toggle = function toggle(element) {
+      var dropdownElement = element.closest('.dropdown');
+      var parentMenuElement = dropdownElement.closest('.dropdown-menu');
+      var menuElement = dropdownElement.querySelector('.dropdown-menu');
+      var isOpen = menuElement.classList.contains('show');
       this.close(parentMenuElement);
-
       menuElement.classList.toggle('show', !isOpen);
-    }
+    };
 
-    close(menuElement) {
-      const menuNodeList = menuElement.querySelectorAll('.dropdown-menu.show');
+    _proto.close = function close(menuElement) {
+      var menuNodeList = menuElement.querySelectorAll('.dropdown-menu.show');
 
-      for (const element of menuNodeList) {
+      for (var _iterator2 = menuNodeList, _isArray2 = Array.isArray(_iterator2), _i2 = 0, _iterator2 = _isArray2 ? _iterator2 : _iterator2[Symbol.iterator]();;) {
+        var _ref2;
+
+        if (_isArray2) {
+          if (_i2 >= _iterator2.length) break;
+          _ref2 = _iterator2[_i2++];
+        } else {
+          _i2 = _iterator2.next();
+          if (_i2.done) break;
+          _ref2 = _i2.value;
+        }
+
+        var element = _ref2;
         element.classList.remove('show');
       }
-    }
-  }
+    };
 
-  // For AMD/Node/CommonJS used elements (optional)
+    return DropdownSubmenu;
+  }(); // For AMD/Node/CommonJS used elements (optional)
   // http://learn.jquery.com/jquery-ui/environments/amd/
-  $.fn.submenupicker = function(elements) {
-    const $elements = this instanceof $ ? this : $(elements);
 
-    return $elements.each(function() {
-      let data = $.data(this, 'bs.submenu');
+
+  $.fn.submenupicker = function (elements) {
+    var $elements = this instanceof $ ? this : $(elements);
+    return $elements.each(function () {
+      var data = $.data(this, 'bs.submenu');
 
       if (!data) {
         data = new DropdownSubmenu(this);
-
         $.data(this, 'bs.submenu', data);
       }
     });
