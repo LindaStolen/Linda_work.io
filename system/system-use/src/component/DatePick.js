3cfd56786e3df0a4a60fbc27bc68ae912451497e
Linda <plusmeow218@gmail.com>
2019-07-15 17:49:42 +0800

    daterangepicker

--- system/system-use/src/component/DatePick.js
+++ system/system-use/src/component/DatePick.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react'
-// import DateTimeRangeContainer from 'react-advanced-datetimerange-picker'
-import { LinkedCalendar } from 'rb-datepicker'
-import {FormControl} from 'react-bootstrap/Form'
-import moment from 'moment'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-// let showTime = {
-//     border: "none",
-//     borderBottom: "1.3px solid #371",
-// }
+import {RangeDatePicker} from "@y0c/react-datepicker"
+// import "@y0c/react-datepicker/assets/styles/calendar.scss"
+import "../css/calendar.scss"
 
 export default class DatePick extends Component {
-    onDatesChange = ({ startDate, endDate }) => {
-        console.log(({ startDate, endDate }));
+    onChange = (date) => {
+        console.log(date);
     }
     render() {
-        // let now = new Date();
-        //     let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0,0));
-        //     let end = moment(start).add(1, "days").subtract(1, "seconds");
-        //     let ranges = {
-        //         "Today Only": [moment(start), moment(end)],
-        //         "Yesterday Only": [moment(start).subtract(1, "days"), moment(end).subtract(1, "days")],
-        //         "3 Days": [moment(start).subtract(3, "days"), moment(end)]
-        //     }
-        //     let local = {
-        //         "format":"DD-MM-YYYY HH:mm",
-        //         "sundayFirst" : false
-        //     }
-        //     let maxDate = moment(start).add(24, "hour")
         return (
             <React.Fragment>
-            <LinkedCalendar onDatesChange={this.onDatesChange} showDropdowns={false} />
+            <RangeDatePicker startText="Start" endText="End" dateFormat="YYYY/MM/DD"/>
             </React.Fragment>
         )
     }
--- system/system-use/src/component/Resault.js
+++ system/system-use/src/component/Resault.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import DatePick from './DatePick'
 import '../css/subnav.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import DatePicker from './DatePicker';
 
 export default class Resault extends Component {
     // consrtuctor(props){
@@ -14,7 +15,7 @@ export default class Resault extends Component {
                 <div className="condition">
                     <div className="navCondition">
                         <span className="spin">BC
-                            <FontAwesomeIcon icon={['fas','times']} style={{marginLeft: "8px",}}/>
+                        <FontAwesomeIcon icon={['fas','times']} style={{marginLeft: "8px",}}/>
                         </span>
                     </div>
                     <DatePick />
