7d91d1de488ea954a85cb6689b96a8a08825ccf9
Linda <plusmeow218@gmail.com>
2019-07-18 09:15:06 +0800

    new daterangepicker & set webpack config

--- system/system-use_1.1/src/component/DatePick.js
+++ system/system-use_1.1/src/component/DatePick.js
@@ -1,56 +1,38 @@
 import React, { Component } from 'react'
-import {RangeDatePicker} from "@y0c/react-datepicker"
-import '../css/calendar.scss'
-import moment from 'moment'
+import { DateRangePicker } from 'rsuite'
+import 'rsuite/styles/less/index.less'
+import 'rsuite/styles/less/main.less'
+// import {RangeDatePicker} from "@y0c/react-datepicker"
+// import '../css/calendar.scss'
+// import moment from 'moment'
+
+let dateFns = new Date();
 
 export default class DatePick extends Component {
-    constructor(props){
-        super(props);
-        let now = new Date();
-        let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate()));
-        let end = moment(now).add(1, "days").subtract(1, "seconds");
-        this.state = {
-            start : start,
-            end : end
-        }
-        this.applyCallback = this.applyCallback.bind(this);
-    }
-    applyCallback(startDate ,endDate){
-        // console.log('apply');
-        console.log(startDate.format('YYYY年MM月DD日'))
-        console.log(endDate.format('YYYY年MM月DD日'))
-        this.setState({
-            newStart: startDate,
-            newEnd: endDate
-        })
-    }
-    // onChange = (date) => {
-    //     console.log(date);
-    // }
+    
     render() {
-        let now = new Date();
-        let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate()));
-        let end = moment(now).add(1, "days").subtract(1, "seconds");
-        let ranges = {
-            'Today Only': [moment(start), moment(end)],
-            'Yesterday Only': [moment(start).subtract(1, 'days'), moment(end).subtract(1, 'days')],
-            '3 Days': [moment(start).subtract(3, 'days'), moment(end)],
-            '5 Days': [moment(start).subtract(5, 'days'), moment(end)],
-            '1 Week': [moment(start).subtract(7, 'days'), moment(end)],
-            '2 Weeks': [moment(start).subtract(14, 'days'), moment(end)],
-            '1 Month': [moment(start).subtract(1, 'months'), moment(end)],
-            '90 Days': [moment(start).subtract(90, 'days'), moment(end)],
-            '1 Year': [moment(start).subtract(1, 'years'), moment(end)],
-          };
-        let maxDate = moment(now).add(24, "hour");
+        const DateRangePickerCustomToolbar = props => (
+            <div className="field">
+              <DateRangePicker
+                ranges={[{
+                  label: '昨天',
+                  value: [dateFns.addDays(new Date(), -1), dateFns.addDays(new Date(), -1)]
+                }, {
+                  label: '今天',
+                  value: [new Date(), new Date()]
+                }, {
+                  label: '明天',
+                  value: [dateFns.addDays(new Date(), 1), dateFns.addDays(new Date(), 1)]
+                }, {
+                  label: '最近 7 天',
+                  value: [dateFns.subDays(new Date(), 6), new Date()]
+                }]}
+              />
+            </div>
+          );
         return (
             <React.Fragment>
-                <RangeDatePicker startText={now} endText={end} dateFormat="YYYY/MM/DD"
-                    ranges={ranges}
-                    start={this.state.start}
-                    end={this.state.end}
-                    applyCallback={this.applyCallback}
-                    maxDate={maxDate}
+                <DateRangePicker style={{DateRangePickerCustomToolbar}}
                 />
             </React.Fragment>
         )
